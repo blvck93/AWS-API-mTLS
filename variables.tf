8c506cb1aa0145c1abe92f3c816280be510a1b95
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-24 06:19:59 +0100

    Variables update - existing VPC and subnets

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "vpc_name" {
-  description = "Value of the Name tag for the EC2 instance"
+variable "vpc-lab_id" {
+  description = "The ID of the existing VPC"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "vpc-089ced31f7481c4bd" 
+}
+
+variable "subnet-lab-1_id" {
+  description = "The ID of the existing subnet"
+  type        = string
+  default     = "subnet-0c8b33e2a2265ae98"  
+}
+
+variable "subnet-lab2_id" {
+  description = "The ID of the existing subnet 2"
+  type        = string
+  default     = "subnet-07c282a6ad15f5cb2"  
 }
