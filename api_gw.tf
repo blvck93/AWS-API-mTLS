91497e195ab582e18faf96e12336d489b14694b7
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-25 11:38:53 +0100

    Add lambda.zip

--- api_gw.tf
+++ api_gw.tf
@@ -16,40 +16,38 @@ resource "aws_api_gateway_domain_name" "api-blvck" {
 }
 
 resource "aws_route53_record" "api-blvck-A" {
-  name    = aws_api_gateway_domain_name.api-blvck.domain_name
+  name    = aws_api_gateway_domain_name.api-blvck.cloudfront_domain_name
   type    = "A"
-  zone_id = data.aws_route53_zone.blvckovh.zone_id
-
+  zone_id = aws_api_gateway_domain_name.api-blvck.zone_id
   alias {
-    name                   = aws_api_gateway_domain_name.api-blvck.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_api_gateway_domain_name.api-blvck.domain_name_configuration[0].hosted_zone_id
+    name                   = aws_api_gateway_domain_name.api-blvck.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api-blvck.zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_api_gateway_authorizer" "lambda" {
-  name                   = "mtls-authorizer"
+  name                   = "LambdaAuthorizer"
   rest_api_id            = aws_api_gateway_rest_api.api.id
   authorizer_uri         = aws_lambda_function.auth_lambda.invoke_arn
-  authorizer_result_ttl_in_seconds = 300
-  type                   = "REQUEST"
+  type                   = "TOKEN"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
-  http_method   = "POST"
-  authorization = "CUSTOM"
-  authorizer_id = aws_api_gateway_authorizer.lambda.id
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_client_certificate" "client_cert" {
+  description = "Client certificate for mTLS"
 }
 
 resource "aws_api_gateway_stage" "stage" {
-  deployment_id = aws_api_gateway_deployment.deploy.id
-  rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deploy.id
   client_certificate_id = aws_api_gateway_client_certificate.client_cert.id
 }
-
 
 resource "aws_api_gateway_base_path_mapping" "mapping" {
   api_id      = aws_api_gateway_rest_api.api.id
