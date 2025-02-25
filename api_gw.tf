098cb4bd6609b91cee65e6ff4ca349f644f207f8
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-25 11:42:59 +0100

    Add lambda.zip

--- api_gw.tf
+++ api_gw.tf
@@ -87,3 +87,28 @@ EOF
     filename = "index.py"
   }
 }
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy" {
+  name       = "lambda_exec_policy"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
