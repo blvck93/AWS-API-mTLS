cd30d4dd090353be769ebb2d654db2f5a7c9b70c
blvck93 <kamil.czarnocki@gmail.com>
2025-02-25 06:36:24 +0100

    Variables update

--- api_gw.tf
+++ api_gw.tf
@@ -86,3 +86,29 @@ def extract_thumbprint(cert):
     return cert[-40:]  # Simulated extraction
 EOF
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda_basic_execution"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
 data "aws_vpc" "vpc-lab" {
-  id = var.vpc_id
+  id = var.vpc_id.id
 }
 
 data "aws_subnet" "subnet-lab-1" {
-  id = var.subnet_id
+  id = var.subnet_id.id
 }
 
 data "aws_subnet" "subnet-lab-2" {
-  id = var.subnet2_id
+  id = var.subnet2_id.id
 }
 
 data "aws_route53_zone" "blvckovh" {
-  id = var.r53zone_id
+  id = var.r53zone_id.id
 }
