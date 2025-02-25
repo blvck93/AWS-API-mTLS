6d3207088685b2b1cd02a5fc9769bacba271df6e
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-25 13:55:17 +0100

    Network change for ec2

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "backend" {
   ami           = "ami-053a45fff0a704a47"  # Update with your AMI
   instance_type = "t3.micro"
   security_groups = [aws_security_group.ec2_sg.name]
+  subnet_id = data.aws_subnet.subnet-lab-1.id
   user_data     = <<-EOF
                 #!/bin/bash
                 sudo yum install -y httpd
