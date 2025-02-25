32ee3f55b3da5f1aec4b46750fd25da549c69f49
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-25 14:06:53 +0100

    Network change for ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "backend" {
   ami           = "ami-053a45fff0a704a47"  # Update with your AMI
   instance_type = "t3.micro"
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   subnet_id = data.aws_subnet.subnet-lab-1.id
   user_data     = <<-EOF
