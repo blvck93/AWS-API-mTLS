5737e70fd8897840284ab39be2e27afa9921ce17
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-02-25 13:47:34 +0100

    Healthchecks fix

--- alb.tf
+++ alb.tf
@@ -11,6 +11,15 @@ resource "aws_lb_target_group" "api_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.vpc-lab.id
+
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    timeout             = 5
+    path                = "/health"  # This path should be implemented in Lambda
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener" "http" {
@@ -28,12 +37,4 @@ resource "aws_lb_target_group_attachment" "api_tg_attachment" {
   target_group_arn = aws_lb_target_group.api_tg.arn
   target_id        = aws_instance.backend.id
   port             = 80
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    timeout             = 5
-    path                = "/health"  # This path should be implemented in Lambda
-    matcher             = "200"
-  }
 }
