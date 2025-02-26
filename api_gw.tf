b232a12ffb320749a2525919de3bbf2420cae084
blvck93 <kamil.czarnocki@gmail.com>
2025-02-26 21:09:21 +0100

    Attach authorizer

--- api_gw.tf
+++ api_gw.tf
@@ -28,13 +28,33 @@ resource "aws_api_gateway_method" "get_method" {
   http_method   = "GET"
   authorization = "NONE"
 }
+resource "aws_api_gateway_authorizer" "lambda_auth" {
+  name                   = "LambdaAuthorizer"
+  rest_api_id            = aws_api_gateway_rest_api.api.id
+  authorizer_uri         = aws_lambda_function.auth_lambda.invoke_arn
+  type                   = "TOKEN"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_auth.id
+}
 
 resource "aws_api_gateway_integration" "alb_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_rest_api.api.root_resource_id
   http_method = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
-  type = "HTTP"
+  type = "AWS_PROXY"
   uri = "http://${aws_lb.api_alb.dns_name}"  
 }
 
