bf0681327ac51714962f17651ef497f469e0695a
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-03-05 06:55:31 +0100

    API GW response method

--- api_gw.tf
+++ api_gw.tf
@@ -45,7 +45,7 @@ resource "aws_api_gateway_integration" "alb_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_rest_api.api.root_resource_id
   http_method = aws_api_gateway_method.get_method.http_method
-  integration_http_method = "POST"
+  integration_http_method = "ANY"
   type = "HTTP"
   uri = "http://${aws_lb.api_alb.dns_name}"  
   
@@ -61,7 +61,12 @@ EOF
   }
 }
 
-
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+}
 
 resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
