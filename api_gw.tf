7dba40c6d31faf4752d440ad310e4ee762522012
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-03-05 06:59:14 +0100

    API GW response method

--- api_gw.tf
+++ api_gw.tf
@@ -68,6 +68,13 @@ resource "aws_api_gateway_method_response" "response_200" {
   status_code = "200"
 }
 
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+}
+
 resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   depends_on = [aws_api_gateway_integration.alb_integration]
