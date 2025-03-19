40417d9ec31d362908f5d7e076ff44bf4ee1d7d2
Kamil Czarnocki <kamil.czarnocki@equiniti.com>
2025-03-19 11:28:25 +0100

    Variables cleanup [skip ci]

--- data.tf
+++ data.tf
@@ -15,17 +15,18 @@ data "aws_acm_certificate" "cert-ext" {
   statuses = ["ISSUED"]
 }
 
-data "aws_s3_object" "truststore_cert" {
-  bucket = "blvck9-c33rts00re2025"
-  key    = "trust-store-cert-github.pem"
-}
-
 data "aws_route53_zone" "blvckovh" {
   name         = "blvck.ovh."
   private_zone = false
 }
 
-# Fetch the S3 object metadata 
+# Fetch the S3 object metadata for TrustStore
+data "aws_s3_object" "truststore_cert" {
+  bucket = "blvck9-c33rts00re2025"
+  key    = "trust-store-cert-github.pem"
+}
+
+# Fetch the S3 object metadata for Lambda
 data "aws_s3_object" "lambda_zip" {
   bucket = "blvck9-c33rts00re2025"
   key    = "lambda_function_nodependencies.zip"
